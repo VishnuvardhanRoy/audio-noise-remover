93cd43b3ce69614cb7891f9e1bf74f04c0abd670
VishnuvardhanRoy <vishnuvardhangov16@gmail.com>
2025-12-25 16:11:59 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,14 +7,11 @@ import os
 
 st.title("🎧 Audio Noise Remover")
 st.write("Upload your noisy audio file below.")
-
-# UPDATED: Added "mpeg" and "ogg" to the allowed list for WhatsApp files
+s
 uploaded_file = st.file_uploader("Choose an audio file", type=["wav", "mp3", "mpeg", "ogg"])
 
 if uploaded_file is not None:
-    # Save the uploaded file temporarily
-    # We name it 'temp_input.mp3' by default to help librosa auto-detect it
-    temp_filename = "temp_input.mp3"
+
     
     with open(temp_filename, "wb") as f:
         f.write(uploaded_file.getbuffer())
@@ -24,13 +21,12 @@ if uploaded_file is not None:
     st.info("Processing... this might take 10-20 seconds...")
 
     try:
-        # Load audio (librosa handles mp3/mpeg/ogg automatically)
         data, rate = librosa.load(temp_filename, sr=None)
 
-        # Remove noise
+     
         reduced_noise = nr.reduce_noise(y=data, sr=rate, stationary=True)
 
-        # Save clean audio
+       
         output_filename = "clean_audio.wav"
         sf.write(output_filename, reduced_noise, rate)
 
@@ -46,8 +42,6 @@ if uploaded_file is not None:
                 file_name="clean_audio.wav",
                 mime="audio/wav"
             )
-
-        # Cleanup
         os.remove(temp_filename)
 
     except Exception as e:
